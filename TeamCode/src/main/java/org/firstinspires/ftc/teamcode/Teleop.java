f22c3147b1e75e2325bf4edf92b41e04a0a5c055
wykzimme <bryce.e.zimmerman@gmail.com>
2025-04-26 09:43:40 -0600

    added teleop chase

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -1,4 +1,33 @@
 package org.firstinspires.ftc.teamcode;
 
-public class Teleop {
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name ="teleop", group = "2024-25 SP")
+//naming it
+public class Teleop extends OpMode {
+    chaseintialization driveteleop = new chaseintialization();
+    armintialization LineraMecanizmeteleop = new armintialization();
+
+    @Override
+    public void init() {
+
+        //calling hardwareMap
+        driveteleop.init(hardwareMap);
+        LineraMecanizmeteleop.init(hardwareMap);
+
+
+
+    }
+
+    @Override
+    public void loop() {
+
+        //seting powers
+        driveteleop.topleft.setPower(-gamepad1.right_stick_y / 1.1 + -gamepad1.left_stick_x * 1.1 + gamepad1.right_stick_x);
+        driveteleop.backleft.setPower(gamepad1.right_stick_y / 1.1 + -gamepad1.left_stick_x * 1.1 -gamepad1.right_stick_x);
+        driveteleop.topright.setPower(-gamepad1.right_stick_y * 1.1 + gamepad1.left_stick_x * 1.2 -gamepad1.right_stick_x);
+        driveteleop.backright.setPower(gamepad1.right_stick_y * 1.1 + gamepad1.left_stick_x * 1.1 + gamepad1.right_stick_x);
+
+    }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/armintialization.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/armintialization.java
@@ -1,4 +1,4 @@
 package org.firstinspires.ftc.teamcode;
 
-public class arminitilazation {
+public class armintialization {
 }
