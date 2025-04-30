4bb89f6020500a33c9e5ebb087a373ec26fca175
wykzimme <bryce.e.zimmerman@gmail.com>
2025-04-29 18:25:07 -0600

    Update chaseintialization.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/chaseintialization.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/chaseintialization.java
@@ -1,4 +1,21 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
 public class chaseintialization {
+
+    public DcMotor topleft;
+    public DcMotor topright;
+    public DcMotor backleft;
+    public DcMotor backright;
+
+    public void init(HardwareMap hwMap){
+
+        topleft = hwMap.get(DcMotor.class, "topleft");
+        topright = hwMap.get(DcMotor.class, "topright");
+        backleft = hwMap.get(DcMotor.class, "backleft");
+        backright = hwMap.get(DcMotor.class, "backright");
+    }
 }
+
